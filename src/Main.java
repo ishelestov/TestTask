0ccf8b7322b14eb431405e0dcee5e911dc343526
Shelestov Ivan <ishelestov@gmail.com>
2015-04-15 11:20:50 +0300

    t3

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Created by vanya on 14.04.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyHashMap myHashMap = new MyHashMap();
+        myHashMap.push(1,111);
+        myHashMap.push(2,222);
+        myHashMap.push(2,333);
+        myHashMap.push(3,444);
+
+    }
+}
